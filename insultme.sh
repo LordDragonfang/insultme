81282a12fef5632fbc4567d54ec10370f73f505a
Sean Sutherland <LordDragonfang@gmail.com>
2019-09-05 17:24:20 -0700

    basic function

--- insultme.sh
+++ insultme.sh
@@ -1,3 +1,48 @@
 #!/usr/bin/env bash
 
+PREFIX=(
+cock
+dick
+fuck
+shit
+piss
+cuck
+slut
+douche
+bitch?
+ass?
+)
 
+PRELEN=${#PREFIX[*]}
+
+SUFFIX=(
+waffle
+muffin
+biscuit
+wheat?
+wad
+weed
+stick
+face
+baby?
+nugget
+bag
+hole
+nozzle
+nuts
+socket?
+)
+
+SUFLEN=${#SUFFIX[*]}
+
+SPECIAL=(
+bitchbaby
+slutmuffin
+buckwheat
+"cock socket"
+fucktrumpet
+"shart goblin"
+cockwaffle
+)
+
+echo ${PREFIX[$RANDOM % $PRELEN]}${SUFFIX[$RANDOM % $SUFLEN]}
